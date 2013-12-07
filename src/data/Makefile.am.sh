c33a6f38e090d4d4ac0179d5c0fd92657141b333
Alejandro Mery <amery@geeks.cl>
2013-12-07 21:36:35 +0100

    nginx: add initial config
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- src/data/Makefile.am.sh
+++ src/data/Makefile.am.sh
@@ -5,11 +5,33 @@ cd "$(dirname "$0")"
 . ../makefile_gen.in
 
 EXTRAFILES="404.html nginx/mime.types"
+CONFFILES="nginx/nginx.conf nginx/nginx-server.conf"
 
 cat <<EOT > Makefile.am
 filesdir = \$(pkgdatadir)
 
+CONFFILES = \\
+	$(list $CONFFILES)
+
+SUBSTFILES = \$(CONFFILES)
+CLEANFILES = \$(SUBSTFILES)
+
 nobase_dist_files_DATA = \\
 	$(list $EXTRAFILES) \\
+	\$(CONFFILES) \\
 	$(listall static)
+
+do_subst = sed \\
+	-e 's,[@]localstatedir[@],\$(localstatedir),g' \\
+	-e 's,[@]pkgdatadir[@],\$(pkgdatadir),g' \\
+	-e 's,[@]pkgname[@],@PACKAGE@,g'
+
+\$(SUBSTFILES):
+	mkdir -p \`dirname \$@\`
+	\$(do_subst) < \$< > \$@~
+	mv \$@~ \$@
+
 EOT
+for x in $CONFFILES; do
+	echo "$x: $x.in"
+done >> Makefile.am
