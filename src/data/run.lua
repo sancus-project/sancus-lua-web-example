4050040284d6dde71e607dae75f35482a19cf424
Alejandro Mery <amery@geeks.cl>
2013-12-08 10:08:41 +0100

    run: initialize lua's random generator
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- src/data/run.lua
+++ src/data/run.lua
@@ -4,4 +4,8 @@ local utils = require"sancus.utils"
 local fastcgi = require"wsapi.fastcgi"
 local app = require"sancuswebexample"
 
+-- Initialize the pseudo random number generator
+math.randomseed(os.time())
+math.random(); math.random(); math.random()
+
 fastcgi.run(app.run)
